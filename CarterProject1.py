b240b14a54805c2d9c9debbe4a44de69dd015f8d
Larson Carter <larsoncarter72404@gmail.com>
2024-10-03 17:01:28 -0500

    Sorta-Pseudo Code
    I started to implement the rougth psuedo code of the entire outline.

--- CarterProject1.py
+++ CarterProject1.py
@@ -17,4 +17,36 @@ def vigenereDecrypt(key, encryptedValue):
     print("Vigenere Decryptor")
 
 if __name__ == '__main__':
-    print("Hello, World!")
+
+    print("Please select a cipher to apply:")
+
+    print("1.\t Keyword Cipher")
+    print("2.\t Columnar Cipher")
+    print("3.\t Vigenere Cipher")
+
+    # Take User Input Here
+
+    print("Please select action:")
+    print("1.\t Encrypt")
+    print("2.\t Decrypt")
+
+    # Take User Input Here
+
+    print("Please provide a keyword:")
+
+    # Take User Input here
+
+    # make this encipher / decipher
+    print("Please provide a text to encipher")
+
+    # Take User Input HERE
+
+    print("Starting SELECTED CIPHERE HERE Cipher")
+
+    # Print the original message that they want to encipher
+
+    # print the enciphereed text
+
+    # loop until the user exits. i.e enter -1????
+
+
