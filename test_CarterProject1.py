2a4d42f0b4145c2abeb5941add262d0c050ce925
Larson Carter <larsoncarter72404@gmail.com>
2024-10-03 22:28:24 -0500

    Cleanup Test
    I cleaned up the test for easier expandability and the room to easily copy and paste them to be more efficient

--- test_CarterProject1.py
+++ test_CarterProject1.py
@@ -3,25 +3,13 @@ from CarterProject1 import (keyword, columnar, vigenere, keywordDecrypt, columna
 
 class Test(TestCase):
     def test_keyword(self):
-        key = "Whale"
-        plaintext = "Hello! I am testing this program."
-        expectedOutput = "Dejjn! F wk sersfmc sdfr oqncqwk."
-        result = keyword(key, plaintext)
-        self.assertEqual(result, expectedOutput, "Keyword cipher encryption failed")
+        self.assertEqual(keyword("Whale", "Hello! I am testing this program."), "Dejjn! F wk sersfmc sdfr oqncqwk.", "Keyword cipher encryption failed")
 
     def test_columnar(self):
-        key = "Whale"
-        plaintext = "Hello! I am testing this program."
-        expectedOutput = "lItnio.oas re ihrml egsgH!mttpa"
-        result = columnar(key, plaintext)
-        self.assertEqual(result, expectedOutput, "Columnar cipher encryption failed")
+        self.assertEqual(columnar("Whale", "Hello! I am testing this program."), "lItnio.oas re ihrml egsgH!mttpa", "Columnar cipher encryption failed")
 
     def test_vigenere(self):
-        key = "Whale"
-        plaintext = "Hello! I am testing this program."
-        expectedOutput = "Dllws! E hm eioaiyk poid tnvgcei."
-        result = vigenere(key, plaintext)
-        self.assertEqual(result, expectedOutput, "Vigenere cipher encryption failed")
+        self.assertEqual(vigenere("Whale", "Hello! I am testing this program."), "Dllws! E hm eioaiyk poid tnvgcei.", "Vigenere cipher encryption failed")
 
     def test_keyword_decrypt(self):
         self.fail()
