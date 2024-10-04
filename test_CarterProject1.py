bfa09620b270553a762f39cd3b137bbf712e5bfb
Larson Carter <larsoncarter72404@gmail.com>
2024-10-03 22:31:34 -0500

    Finish Decrypt Test
    I finished implementing the decrypt Unit Testing.

--- test_CarterProject1.py
+++ test_CarterProject1.py
@@ -12,10 +12,10 @@ class Test(TestCase):
         self.assertEqual(vigenere("Whale", "Hello! I am testing this program."), "Dllws! E hm eioaiyk poid tnvgcei.", "Vigenere cipher encryption failed")
 
     def test_keyword_decrypt(self):
-        self.fail()
+        self.assertEqual(keywordDecrypt("Whale", "Dejjn! F wk sersfmc sdfr oqncqwk."), "Hello! I am testing this program.", "Keyword cipher decrypter failed")
 
     def test_columnar_decrypt(self):
-        self.fail()
+        self.assertEqual(columnarDecrypt("Whale", "lItnio.oas re ihrml egsgH!mttpa"), "Hello! I am testing this program.", "Columnar cipher decrypter failed")
 
     def test_vigenere_decrypt(self):
-        self.fail()
+        self.assertEqual(vigenereDecrypt("Whale", "Dllws! E hm eioaiyk poid tnvgcei."), "Hello! I am testing this program.", "Vigenere cipher decrypter failed")
